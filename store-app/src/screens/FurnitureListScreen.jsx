a90a4679257b1c41cd4cc29074aa732bfab4c1e7
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-28 20:07:26 +0200

    add base list screen

---
+++ store-app/src/screens/FurnitureListScreen.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function FurnitureListScreen() {
+  return (
+    <View>
+      <Text>Furniture list is empty</Text>
+    </View>
+  );
+}
