a4a8122dccd27cd4ca533cb0725f2be9893e5cb4
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-28 20:05:08 +0200

    add app navigator

---
+++ store-app/src/navigation/AppNavigator.jsx
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import DrawerNavigator from './DrawerNavigator.jsx';
+
+export default function AppNavigator() {
+
+    const Stack = createNativeStackNavigator();
+    
+     return (
+        <Stack.Navigator 
+            screenOptions={{
+                headerShown: false, 
+            }}
+        >
+
+            <Stack.Screen name="app-s" component={DrawerNavigator} />
+
+        </Stack.Navigator>
+    );
+}
