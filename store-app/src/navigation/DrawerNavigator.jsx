b477cd9793fb2bef9f8f20248b41ba2301508463
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-28 20:05:47 +0200

    add template for drawer nav

---
+++ store-app/src/navigation/DrawerNavigator.jsx
@@ -0,0 +1,30 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import BottomTabsNavigator from './BottomTabsNavigator';
+
+// import ProfileScreen from '../screens/ProfileScreen';
+// import OrdersScreen from '../screens/OrdersScreen';
+// import SettingsScreen from '../screens/SettingsScreen';
+import InfoScreen from '../screens/InfoScreen';
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      
+      screenOptions={{
+        drawerPosition: "right",
+        headerShown: false, 
+        drawerType: 'front', 
+      }}
+    >
+      
+      <Drawer.Screen name="MainTabs" component={BottomTabsNavigator} />
+      
+      {/* <Drawer.Screen name="Profile" component={ProfileScreen} />
+      <Drawer.Screen name="Orders" component={OrdersScreen} />
+      <Drawer.Screen name="Settings" component={SettingsScreen} /> */}
+      <Drawer.Screen name="Info" component={InfoScreen} />
+    </Drawer.Navigator>
+  );
+}
