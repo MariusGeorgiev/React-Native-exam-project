8c1eab2b8d1d885ef6aff96afa90738cd9d6d567
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-28 20:05:59 +0200

    add template for tabs nav

---
+++ store-app/src/navigation/BottomTabsNavigator.jsx
@@ -0,0 +1,23 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeStackNavigator from './HomeStackNavigator';
+import CategoriesStackNavigator from './CategoriesStackNavigator';
+// import FavoritesStackNavigator from './FavoritesStackNavigator';
+// import CartStackNavigator from './CartStackNavigator';
+import DrawerButton from '../components/DrawerButton';
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabsNavigator() {
+  return (
+    <Tab.Navigator screenOptions={{
+        headerTitleAlign: 'left',   
+        headerRight: () => <DrawerButton />, 
+      }}>
+      <Tab.Screen name="Home" component={HomeStackNavigator} />
+      <Tab.Screen name="Categories" component={CategoriesStackNavigator} />
+      {/* <Tab.Screen name="Favorites" component={FavoritesStackNavigator} />
+      <Tab.Screen name="Cart" component={CartStackNavigator} /> */}
+    </Tab.Navigator>
+  );
+}
