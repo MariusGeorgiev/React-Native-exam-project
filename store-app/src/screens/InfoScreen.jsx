de38c14371754758a10050fc45c02589a13004ce
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-03-02 18:01:55 +0200

    add safe area to info page in drawer

--- store-app/src/screens/InfoScreen.jsx
+++ store-app/src/screens/InfoScreen.jsx
@@ -1,9 +1,12 @@
 import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function InfoScreen() {
   return (
+    <SafeAreaView style={{ flex: 1 }}>
     <View>
       <Text>Info Screen....</Text>
     </View>
+    </SafeAreaView>
   );
 }
