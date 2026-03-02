3b5524e8800c1a66fc50ef11c297d00600e6c598
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-03-02 20:36:35 +0200

    add base stack nav for favorites

---
+++ store-app/src/navigation/FavoritesStackNavigator.jsx
@@ -0,0 +1,24 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import FavoritesScreen from '../screens/FavoritesScreen';
+// import FurnitureDetailsScreen from '../screens/FurnitureDetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function FavoritesStackNavigator() {
+  return (
+    <Stack.Navigator
+      
+    >
+      {/* <Stack.Screen
+        name="FavoritesScreen"
+        component={FavoritesScreen}
+        options={{ title: 'Favorites' }}
+      />
+      <Stack.Screen
+        name="FurnitureDetails"
+        component={FurnitureDetailsScreen}
+        options={{ title: 'Details' }}
+      /> */}
+    </Stack.Navigator>
+  );
+}
