62848a9e3bcb489b09f9e368778b5d181ea45bec
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-03-03 00:14:16 +0200

    add firebase configuration file

---
+++ store-app/src/firebase/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBU5qttj-ZDrH2tz9zPVcV2-9ah677UyJM",
+  authDomain: "furniturestoreapp-react-native.firebaseapp.com",
+  projectId: "furniturestoreapp-react-native",
+  storageBucket: "furniturestoreapp-react-native.firebasestorage.app",
+  messagingSenderId: "136190598494",
+  appId: "1:136190598494:web:2062a18a7d00a77e9143cf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
