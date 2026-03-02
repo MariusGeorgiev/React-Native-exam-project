f18d86b9d7cb9d2f8b173938aa7c5c19bc04fa6d
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-03-02 20:35:39 +0200

    add icons for bottom tabs

--- store-app/src/navigation/BottomTabsNavigator.jsx
+++ store-app/src/navigation/BottomTabsNavigator.jsx
@@ -2,9 +2,10 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeStackNavigator from './HomeStackNavigator';
 import CategoriesStackNavigator from './CategoriesStackNavigator';
-// import FavoritesStackNavigator from './FavoritesStackNavigator';
-// import CartStackNavigator from './CartStackNavigator';
+import FavoritesStackNavigator from './FavoritesStackNavigator';
+import CartStackNavigator from './CartStackNavigator';
 import DrawerButton from '../components/DrawerButton';
+import { Ionicons } from '@expo/vector-icons'; 
 
 const Tab = createBottomTabNavigator();
 
@@ -17,11 +18,34 @@ export default function BottomTabsNavigator() {
         headerRight: () => <DrawerButton />, 
       }}
       >
-      <Tab.Screen name="Home" component={HomeStackNavigator} />
-      <Tab.Screen name="Categories" component={CategoriesStackNavigator} 
+      <Tab.Screen name="Home" component={HomeStackNavigator} 
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home-outline" size={size} color={color} />
+              ),
+        }}
+        />
+      <Tab.Screen name="Categories" component={CategoriesStackNavigator}
+            options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="list-outline" size={size} color={color} />
+                ),
+              }} 
       />
-      {/* <Tab.Screen name="Favorites" component={FavoritesStackNavigator} />
-      <Tab.Screen name="Cart" component={CartStackNavigator} /> */}
+      <Tab.Screen name="Favorites" component={FavoritesStackNavigator} 
+            options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="heart-outline" size={size} color={color} />
+                ),
+              }}
+        />
+      <Tab.Screen name="Cart" component={CartStackNavigator} 
+            options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="cart-outline" size={size} color={color} />
+                ),
+              }}
+      />
     </Tab.Navigator>
   );
 }
