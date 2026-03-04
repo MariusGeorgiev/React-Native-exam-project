0eec199d52f0aa7de520c226d528d5a1d25a954e
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-03-04 04:36:48 +0200

    import custom headers and use in category stack

--- store-app/src/navigation/CategoriesStackNavigator.jsx
+++ store-app/src/navigation/CategoriesStackNavigator.jsx
@@ -2,17 +2,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import FurnitureListScreen from '../screens/FurnitureListScreen';
 import FurnitureDetailsScreen from '../screens/FurnitureDetailsScreen';
-import { View, Text, StyleSheet } from 'react-native';
+// import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import { SimpleHeader, BackHeader } from '../components/headers';
 
 const Stack = createNativeStackNavigator();
 
-function CustomHeader({ title }) {
-  return (
-    <View style={styles.header}>
-      <Text style={styles.headerText}>{title}</Text>
-    </View>
-  );
-}
 
 export default function CategoriesStackNavigator() {
   return (
@@ -27,35 +21,32 @@ export default function CategoriesStackNavigator() {
       <Stack.Screen 
         name="CategoriesScreen" 
         component={CategoriesScreen} 
-        options={{  headerShown: false, }}
+        options={{ header: () => <SimpleHeader title='Categories' /> }}
       />
       <Stack.Screen 
         name="FurnitureList" 
         component={FurnitureListScreen} 
-        options={({ route }) => ({
-          header: () => <CustomHeader title={`${route.params.categoryTitle} › ${route.params.subcategory}`} />,
+        options={({ route, navigation }) => ({
+          header: () => <BackHeader title={`${route.params.categoryTitle} › ${route.params.subcategory}`} navigation={navigation}/>,
           
         })}
       />
       <Stack.Screen 
         name="FurnitureDetails" 
         component={FurnitureDetailsScreen} 
-        options={{ title: 'Details' }}
+          options={({ navigation }) => ({
+          header: () => (
+            <BackHeader
+              title="Details"
+              navigation={navigation}
+            />
+          ),
+        })}
       />
     </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  header: {
-    height: 40,
-    backgroundColor: '#b09999',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  headerText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-});
+// const styles = StyleSheet.create({
+  
+// });
