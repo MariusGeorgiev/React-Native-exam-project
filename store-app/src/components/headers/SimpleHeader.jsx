07010abea93d5559ee81d11ff3f899088778e19b
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-03-04 04:31:02 +0200

    add simple header without back btn

---
+++ store-app/src/components/headers/SimpleHeader.jsx
@@ -0,0 +1,34 @@
+import { View, Text, StyleSheet } from 'react-native';
+import DrawerButton from '../DrawerButton';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function SimpleHeader({ title }) {
+  return (
+    <SafeAreaView edges={['top']} style={styles.safe}>
+    <View style={styles.header}>
+      <Text style={styles.title} numberOfLines={1}>
+        {title}
+      </Text>
+
+      <DrawerButton style={{ justifyContent: 'center', }} />
+    </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    height: 44,
+    backgroundColor: '#b09999',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: 15,
+    paddingRight: 8,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
