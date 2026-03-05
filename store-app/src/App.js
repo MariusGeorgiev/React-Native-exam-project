91b51b8d6ca354e2a1ac036301cb068776700df3
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-03-05 05:48:17 +0200

    wrap app with provider

--- store-app/src/App.js
+++ store-app/src/App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
-
+import { AuthProvider } from './contexts/AuthProvider';
 
 export default function App() {
   return (
+    
     <NavigationContainer>
-        <StatusBar style="auto" />
-        <AppNavigator />
+
+        <AuthProvider>
+            <StatusBar style="auto" />
+            <AppNavigator />
+        </AuthProvider>
 
     </NavigationContainer>
-    
+   
   );
 }
 
