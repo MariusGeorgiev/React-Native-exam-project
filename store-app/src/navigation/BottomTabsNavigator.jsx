13ad09f5270de27a568e80fe7b7e3826560e77e8
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-03-05 05:49:50 +0200

    hide bottom tabs if not logged

--- store-app/src/navigation/BottomTabsNavigator.jsx
+++ store-app/src/navigation/BottomTabsNavigator.jsx
@@ -1,83 +1,93 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import HomeStackNavigator from './HomeStackNavigator';
 import CategoriesStackNavigator from './CategoriesStackNavigator';
 import FavoritesStackNavigator from './FavoritesStackNavigator';
 import CartStackNavigator from './CartStackNavigator';
-import DrawerButton from '../components/DrawerButton';
-import { Ionicons } from '@expo/vector-icons';
-import { SimpleHeader, BackHeader } from '../components/headers';
 import AuthStackNavigator from './AuthStackNavigator';
-
 import CreateFurnitureScreen from '../screens/CreateFurnitureScreen';
+import { SimpleHeader } from '../components/headers';
+
+import { useAuth } from '../contexts/AuthProvider';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabsNavigator() {
+  const { user, loading } = useAuth();
+
+  if (loading) return null;
 
   return (
-    <Tab.Navigator screenOptions={{
-      headerShown: true, // need to remove later
-        // headerTitleAlign: 'left',   
-        // headerRight: () => <DrawerButton />, 
-      }}
-      >
-      <Tab.Screen name="Home" component={HomeStackNavigator} 
-            options={{
-              headerShown: false,
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="home-outline" size={size} color={color} />
-              ),
-        }}
-        />
-      <Tab.Screen name="Categories" component={CategoriesStackNavigator}
-            options={{
-              headerShown: false,
-                tabBarIcon: ({ color, size }) => (
-                  <Ionicons name="list-outline" size={size} color={color} />
-                ),
-              }} 
-      />
-      <Tab.Screen name="Favorites" component={FavoritesStackNavigator} 
-            options={{
-              headerShown: false,
-                tabBarIcon: ({ color, size }) => (
-                  <Ionicons name="heart-outline" size={size} color={color} />
-                ),
-              }}
-        />
-      <Tab.Screen name="Cart" component={CartStackNavigator} 
-            options={{
-              headerShown: false,
-                tabBarIcon: ({ color, size }) => (
-                  <Ionicons name="cart-outline" size={size} color={color} />
-                ),
-              }}
-      />
-
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      
       <Tab.Screen
-  name="Login"
-  component={AuthStackNavigator}
-  options={{
-    headerShown: false,
-    tabBarIcon: ({ color, size }) => (
-      <Ionicons name="log-in-outline" size={size} color={color} />
-    ),
-  }}
-/>
-
-      <Tab.Screen
-        name="Add"
-        component={CreateFurnitureScreen}
+        name="Home"
+        component={HomeStackNavigator}
         options={{
-          header: () => <SimpleHeader title='Create new Furniture'/>,
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="add-circle-outline" size={size} color={color} />
+            <Ionicons name="home-outline" size={size} color={color} />
           ),
         }}
-        
-    />
+      />
 
+      <Tab.Screen
+        name="Categories"
+        component={CategoriesStackNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="list-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      {!user && (
+        <Tab.Screen
+          name="Login"
+          component={AuthStackNavigator}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="log-in-outline" size={size} color={color} />
+            ),
+          }}
+        />
+      )}
+
+      {user && (
+        <>
+          <Tab.Screen
+            name="Favorites"
+            component={FavoritesStackNavigator}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="heart-outline" size={size} color={color} />
+              ),
+            }}
+          />
+
+          <Tab.Screen
+            name="Cart"
+            component={CartStackNavigator}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="cart-outline" size={size} color={color} />
+              ),
+            }}
+          />
+
+          <Tab.Screen
+            name="Add"
+            component={CreateFurnitureScreen}
+            options={{
+              headerShown: true,
+              header: () => <SimpleHeader title="Create new Furniture" />,
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="add-circle-outline" size={size} color={color} />
+              ),
+            }}
+          />
+        </>
+      )}
     </Tab.Navigator>
   );
 }
