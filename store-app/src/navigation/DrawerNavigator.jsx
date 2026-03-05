e3f023e3e4b9761a017730c1fe71cb108a29e518
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-03-05 05:52:58 +0200

    use custom drawer in drawerNav

--- store-app/src/navigation/DrawerNavigator.jsx
+++ store-app/src/navigation/DrawerNavigator.jsx
@@ -1,31 +1,30 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import BottomTabsNavigator from './BottomTabsNavigator';
 
 // import ProfileScreen from '../screens/ProfileScreen';
 // import OrdersScreen from '../screens/OrdersScreen';
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import BottomTabsNavigator from './BottomTabsNavigator';
+
 import SettingsScreen from '../screens/SettingsScreen';
 import InfoScreen from '../screens/InfoScreen';
+import CustomDrawerContent from '../components/CustomDrawerContent';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigator() {
   return (
     <Drawer.Navigator
-      
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
       screenOptions={{
-        drawerPosition: "right",
-        headerShown: false, 
+        drawerPosition: 'right',
+        headerShown: false,
         drawerType: 'front',
         drawerStyle: {
-        width: '55%',
-    },
+          width: '55%',
+        },
       }}
     >
-      
       <Drawer.Screen name="MainTabs" component={BottomTabsNavigator} />
-      
-      {/* <Drawer.Screen name="Profile" component={ProfileScreen} /> */}
-      {/* <Drawer.Screen name="Orders" component={OrdersScreen} /> */}
       <Drawer.Screen name="Settings" component={SettingsScreen} />
       <Drawer.Screen name="Info" component={InfoScreen} />
     </Drawer.Navigator>
