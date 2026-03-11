8eadc01756243a4ece937229e7438858116c4ee5
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-03-11 22:55:40 +0200

    create basic order screen

---
+++ store-app/src/screens/OrdersScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function OrdersScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+    <View>
+      <Text>Orders Screen....</Text>
+    </View>
+    </SafeAreaView>
+  );
+}
